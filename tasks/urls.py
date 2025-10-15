060f9bfdd313643e85dc29771b91ddde9cd5a80e
ramonparaiba <ramonparaiba@gmail.com>
2025-10-15 14:28:56 -0300

    Setup the project

---
+++ tasks/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from tasks import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+router = routers.DefaultRouter()
+router.register(r'tasks', views.TaskViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
